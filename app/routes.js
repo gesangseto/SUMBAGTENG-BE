21d0348b163aa416a282e3d9dfa3884c9a909780
gesangseto <gesangseto@gmail.com>
2021-10-10 21:52:06 +0700

    Remove unused route

--- app/routes.js
+++ app/routes.js
@@ -1,53 +1,6 @@
 "use strict";
 
 module.exports = function (app) {
-  var login = require("./controller/login");
-  app.route("/api/login/user").post(login.user_login);
-
-  var audit_log = require("./controller/audit_log");
-  app.route("/api/audit/log").get(audit_log.get);
-
-  var menu = require("./controller/menu");
-  app.route("/api/menu/user").get(menu.user_menu);
-
-  var sys_configuration = require("./controller/sys_configuration");
-  app.route("/api/configuration").get(sys_configuration.get);
-  app.route("/api/configuration").post(sys_configuration.update);
-
-  var sys_menu_parent = require("./controller/sys_menu_parent");
-  app.route("/api/menu/parent").get(sys_menu_parent.get);
-  app.route("/api/menu/parent").put(sys_menu_parent.insert);
-  app.route("/api/menu/parent").post(sys_menu_parent.update);
-  app.route("/api/menu/parent").delete(sys_menu_parent.delete);
-
-  var sys_menu_child = require("./controller/sys_menu_child");
-  app.route("/api/menu/child").get(sys_menu_child.get);
-  app.route("/api/menu/child").put(sys_menu_child.insert);
-  app.route("/api/menu/child").post(sys_menu_child.update);
-  app.route("/api/menu/child").delete(sys_menu_child.delete);
-
-  var sys_role_section = require("./controller/sys_role_section");
-  app.route("/api/role/section").get(sys_role_section.get);
-  app.route("/api/role/section").post(sys_role_section.update);
-
-  var user_department = require("./controller/user_department");
-  app.route("/api/master/user_department").get(user_department.get);
-  app.route("/api/master/user_department").put(user_department.insert);
-  app.route("/api/master/user_department").post(user_department.update);
-  app.route("/api/master/user_department").delete(user_department.delete);
-
-  var user_section = require("./controller/user_section");
-  app.route("/api/master/user_section").get(user_section.get);
-  app.route("/api/master/user_section").put(user_section.insert);
-  app.route("/api/master/user_section").post(user_section.update);
-  app.route("/api/master/user_section").delete(user_section.delete);
-
-  var user = require("./controller/user");
-  app.route("/api/master/user").get(user.get);
-  app.route("/api/master/user").put(user.insert);
-  app.route("/api/master/user").post(user.update);
-  app.route("/api/master/user").delete(user.delete);
-
   var report_notes = require("./controller/report_notes");
   app.route("/api/report/note").get(report_notes.get);
   app.route("/api/report/note").put(report_notes.insert);
